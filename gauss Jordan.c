bf609bea15acfb65c9a455bee6e4fe37316a95f9
Manisha Basera <91174169+manisha1928@users.noreply.github.com>
2022-10-10 01:09:17 +0530

    Create gauss Jordan.c

---
+++ gauss Jordan.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include<math.h>
+#include<conio.h>
+
+int main()
+{
+  int i,j,k,n;
+  float a[10][10], x[10],ratio;
+  
+  printf("Enter the order of matrix:\n");
+  scanf("%d",&n);
+  printf("enter the elements of the augumented matrix\n");
+  for( i=1;i<=n;i++)
+  {
+      for( j=1;j<=n+1;j++)
+      {
+          printf("a[%d][%d]=\t",i,j);
+          scanf("%f",&a[i][j]);
+      }
+   }
+  for(i=1;i<=n;i++)
+  {
+      if(a[i][i]==0.0)
+      {
+          printf("Mathematical error\n");
+          exit(0);
+      }
+      for(j=1;j<=n;j++)
+      {
+          if(j!=i)
+      {
+          ratio= a[j][i]/a[i][i];
+          for(k=1;k<=n+1;k++)
+          {
+              a[j][k]=a[j][k]-ratio*a[i][k];
+          }
+      } 
+      }
+     
+  }
+  for(i=1;i<=n;i++)
+  {
+      x[i]=a[i][n+1]/a[i][i];
+  }
+  for(i=1;i<=n;i++)
+  {
+      printf("x[%d]=%0.3f\n",i,x[i]);
+  }
+  getch();
+}
